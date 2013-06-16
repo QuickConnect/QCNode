d0a4d417e88c2838eb4f3eb1bb70e4a09b59d8c2
joshmbarney <joshmbarney@gmail.com>
2013-06-16 11:15:27 -0600

    validation failures are now stacked up and returned at the end of processing the valcfs. Several other updates. Added the begining of tests. More to come.

--- QuickConnect.js
+++ QuickConnect.js
@@ -1,157 +1,116 @@
 var Mapper = require('./Mapper'),
-	Stack = require('./Stack').Stack
+    Stack = require('./Stack').Stack
 
 function QuickConnect(testing, debug) {
-	var mapper, executionMap, debug, fakeQC
+    var mapper, executionMap, debug, fakeQC
 
-	this.WAIT_FOR_DATA = 'wAiT'
-	this.STACK_EXIT = 'ExIt_StAcK'
-	this.STACK_CONTINUE = true
-	
-	mapper = new Mapper
-	this.mapper = mapper
-	
-	debug = debug || console.log
-	this.debug = debug
-	
-	function blah(){
-    var immediateExists = true
-    try{
-      setImmediate(function(){})
-    } catch (e) {
-      immediateExists = false
-    }
-    this.nextTick = function (fn, prefereNextTick) {
-      if (prefereNextTick || !immediateExists) {
-        process.nextTick(fn)
-      } else {
-        setImmediate(fn)
-      }
-    }
-  }
-  blah.call(this)
-	
-	fakeQC = (function (self) {
-  	return {
-  		WAIT_FOR_DATA : 'wAiT',
-  		STACK_EXIT : 'ExIt_StAcK',
-  		STACK_CONTINUE : true,
-  		handleRequest: function () {
-			return handleRequest.apply(self, [].slice.call(arguments,0))
-		},
-  		handleRequests: function () {
-			return handleRequests.apply(self, [].slice.call(arguments,0))
-		},
-  		checkForStack: function(){
-  		    return checkForStack.apply(self, [].slice.call(arguments,0))
-  		},
-  		debug: self.debug,
-  		nextTick: self.nextTick
-  	}
-  })(this)
-	
-	executionMap = {}
-	
-	function genrateUUID() {
-	  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g
-	    , function(c) {
-	    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
-	    return v.toString(16)
-	  })
-	  return uuid
-	}
-	
-	function handleRequest(aCmd, requestData, callbacks/*, runBackground*/) {
-		var stack, uuid, funcs, event
-		if (callbacks && callbacks.constructor != Object) {
-		    callbacks = { 'end': callbacks }
-		}
-		uuid = genrateUUID()
-		funcs = cloneConsumableStacks(aCmd, uuid)
-		if (!funcs) {
-			throw new Error('Attempting to execute the command "'
-			  + (aCmd || 'missing')+'" for which no control functions are mapped.')
-		}
-		stack = new Stack(uuid, funcs, requestData, fakeQC, testing)
-		for (event in callbacks) {
-		    stack.on( event, callbacks[event] )
-		}
-		this.nextTick(stack.go)
-		return stack
-	}
-	this.handleRequest = handleRequest
-	
-//	function handleRequests(aCommandArray, requestData
-//	                    , allStacksCompleteCallback, runParallel/*, runBackground*/) {
-//	    
-//	    var uuid, i, count, aCmd, stacks = [], oldStack, stack, tail
-//	    
-//		if (!aCommandArray || aCommandArray.length == 0){
-//		  console.warn('WARNING: attempting to execute a request without one or more commands.')
-//		}
-//		if (!Array.isArray(aCommandArray)) {
-//		  aCommandArray = [aCommandArray]
-//		}
-//		
-//		aCommandArray = aCommandArray.slice()
-//		
-//		for (i = 0, count = aCommandArray.length; i < count; i++) {
-//			uuid = genrateUUID()
-//			aCmd = aCommandArray[i]
-//			funcs = cloneConsumableStacks(aCmd, uuid)
-//			if (!funcs) {
-//				console.warn('WARNING: attempting to execute the command "'
-//				  + (aCommandArray[0] || 'missing')+'" for which no control functions are mapped.')
-//				  return
-//			}
-//			stack = new Stack(uuid, funcs, requestData, fakeQC, callback, testing)
-//			if (oldStack && !runParallel) {
-//				oldStack.next = stack
-//			}
-//			stacks.push(stack)
-//		}
-//		this.nextTick(function () {
-//			stacks[0].go()
-//		})
-//		return stacks
-//	}
-//	this.handleRequest = handleRequest
-	
-	function cloneConsumableStacks(aCmd, uuid){
-	
-	  //var aCmd = aCommandArray.shift()
-	  //debug('cloning: ')
-	  var funcs = {
-	  	"validationMapConsumables": {},
-	  	"dataMapConsumables": {},
-	  	"viewMapConsumables": {}
-	  }
-	  //debug("Command: "+aCmd)
-	  //if mappings are found then duplicate the mapped 
-	  //control function arrays for consumption
-	  if (!mapper.validationMap[aCmd] && !mapper.dataMap[aCmd] 
-	          && !mapper.viewMap[aCmd]) {
-	    //debug("returning null as the command")
-	    return
-	  }
-	
-	  funcs.validationMapConsumables[uuid] = (mapper.validationMap[aCmd] || [] ).slice()
-	  funcs.dataMapConsumables[uuid] = (mapper.dataMap[aCmd] || [] ).slice()
-	  funcs.viewMapConsumables[uuid] = (mapper.viewMap[aCmd] || [] ).slice()
-	  
-	  return funcs
-	}
+    this.WAIT_FOR_DATA = 'wAiT'
+    this.STACK_EXIT = 'ExIt_StAcK'
+    this.STACK_CONTINUE = true
 
-	function checkForStack(stackName){
-		return this.mapper.checkForStack(stackName)
-	}
-	this.checkForStack = checkForStack
-	
-	function command(command, callback) {
-		this.mapper.command(command, callback)
-	}
-	this.command = command
+    mapper = new Mapper
+    this.mapper = mapper
+
+    debug = debug || console.log
+    this.debug = debug
+
+    function blah() {
+        var immediateExists = true
+        try {
+            setImmediate(function () {})
+        } catch (e) {
+            immediateExists = false
+        }
+        this.nextTick = function (fn, prefereNextTick) {
+            if (prefereNextTick || !immediateExists) {
+                process.nextTick(fn)
+            } else {
+                setImmediate(fn)
+            }
+        }
+    }
+    blah.call(this)
+
+    fakeQC = (function (self) {
+        return {
+            WAIT_FOR_DATA: 'wAiT',
+            STACK_EXIT: 'ExIt_StAcK',
+            STACK_CONTINUE: true,
+            handleRequest: function () {
+                return handleRequest.apply(self, [].slice.call(arguments, 0))
+            },
+            handleRequests: function () {
+                return handleRequests.apply(self, [].slice.call(arguments, 0))
+            },
+            checkForStack: function () {
+                return checkForStack.apply(self, [].slice.call(arguments, 0))
+            },
+            debug: self.debug,
+            nextTick: self.nextTick
+        }
+    })(this)
+
+    executionMap = {}
+
+    function genrateUUID() {
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0,
+                v = c == 'x' ? r : (r & 0x3 | 0x8)
+                return v.toString(16)
+        })
+        return uuid
+    }
+
+    function handleRequest(aCmd, requestData, callbacks ) {
+        var stack, uuid, funcs, event
+        if (callbacks && callbacks.constructor != Object) {
+            callbacks = {
+                'end': callbacks
+            }
+        }
+        uuid = genrateUUID()
+        funcs = cloneConsumableStacks(aCmd)
+        if (!funcs) {
+            throw new Error('Attempting to execute the command "' + (aCmd || 'missing') + '" for which no control functions are mapped.')
+        }
+        stack = new Stack(uuid, funcs, requestData, fakeQC, testing)
+        for (event in callbacks) {
+            stack.on(event, callbacks[event])
+        }
+        this.nextTick(stack.go)
+        return stack
+    }
+    this.handleRequest = handleRequest
+    this.run = handleRequest
+
+
+    function cloneConsumableStacks(aCmd) {
+
+        var funcs = {
+        }
+
+        //if mappings are found then duplicate the mapped 
+        //control function arrays for consumption
+        if (!mapper.validationMap[aCmd] && !mapper.dataMap[aCmd] && !mapper.viewMap[aCmd]) {
+            return
+        }
+
+        funcs.validationMapConsumables = (mapper.validationMap[aCmd] || []).slice()
+        funcs.dataMapConsumables = (mapper.dataMap[aCmd] || []).slice()
+        funcs.viewMapConsumables = (mapper.viewMap[aCmd] || []).slice()
+
+        return funcs
+    }
+
+    function checkForStack(stackName) {
+        return this.mapper.checkForStack(stackName)
+    }
+    this.checkForStack = checkForStack
+
+    function command(command, callback) {
+        this.mapper.command(command, callback)
+    }
+    this.command = command
 }
 exports.QuickConnect = QuickConnect
 exports.sharedQC = new QuickConnect
-
---
+++ tests/test.js
@@ -0,0 +1,44 @@
+var resolve = require('path').resolve,
+    mod = require(resolve(__dirname,'../')),
+	QuickConnect = mod.QuickConnect,
+	fs = require('fs')
+	
+test = new QuickConnect
+
+test.command("running all the tests", function(){
+    this.dcf(function(data, qc){
+        fs.readdir(resolve(__dirname,'testers'),function(err, files){
+            var index
+            if (err) {
+                qc.asyncStackError(err)
+            } else {
+                qc.asyncStackContinue('files',files)
+            }
+        })
+        return qc.WAIT_FOR_DATA
+    })
+    this.dcf(function(data, qc){
+        var i, count
+        
+        count = data.files.length
+        for (i = 0; i < count; i++) {
+        (function(){
+            var file = data.files[i]
+            var tester = require(resolve(__dirname,'testers',file))
+            tester.run('run all tests',{},{
+                end: function(data){
+                    console.log('finished tests in '+file)
+                    qc.asyncStackContinue()
+                },
+                error: function(err, data){
+                    console.log('error for tests in '+file+':\n\n'+err.stack)
+                    qc.asyncStackContinue()
+                }
+            })
+        }).call(this)
+        }
+        return qc.WAIT_FOR_DATA
+    })
+})
+
+test.run('running all the tests',{})
